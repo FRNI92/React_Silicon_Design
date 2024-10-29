b57a0c1dd7cadd27993fe918f09b323383602b89
fredrik <freedrik92@hotmail.com>
2024-10-29 17:13:43 +0100

    har gjort så att dropdownfunctionen fungerar igen, den är dock hackig. har hämtat API till FAQ och fått fram det korrekt

--- src/Components/DropDownContainer.jsx
+++ src/Components/DropDownContainer.jsx
@@ -1,63 +1,46 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 const DropDownContainer = () => {
+
+    const [accordionData, setAccordionData] = useState([])
+    const [openAccordion, setOpenAccordion] = useState(null)
+    
+    useEffect(() => {
+      fetch('https://win24-assignment.azurewebsites.net/api/faq')
+      .then(response => response.json())
+      .then(data => setAccordionData(data))
+      .catch(error => console.error('Error fetching data:', error))
+    },[])
+
+    const toggleAccordion = (id) => {
+      setOpenAccordion(openAccordion === id ? null : id); // Växlar mellan att öppna och stänga
+ 
+  }
+
   return (
     <section className="drop-down-menu-container">
-
-        <div className="accordion-item">
-          <div className="accordion">
-            Is any of my personal information stored in the App?
-          </div>
-          <div className="panel">
-            <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <div className="accordion">
-            What formats can I download my transaction history in?
-          </div>
-          <div className="panel">
-            <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-          </div>
-        </div>
-          <div className="accordion-item">
-            <div className="accordion">
-              Can I schedule future transfers?
+        {accordionData.map((item) => 
+          <div key={item.id} className="accordion-item">
+            <div className={`accordion ${openAccordion === item.id ? 'active' : ''}`}
+              onClick={() => toggleAccordion(item.id)}>
+                {item.title} 
             </div>
-            <div className="panel">
-              <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
+            <div className={`panel ${openAccordion === item.id ? 'show' : ''}`}>
+                <p>{item.content}</p>
             </div>
           </div>
-          <div className="accordion-item">
-            <div className="accordion">
-              When can I use Banking App services?
-            </div>
-            <div className="panel">
-              <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <div className="accordion">
-              Can I create my own password that is easy for me to remember?
-            </div>
-            <div className="panel">
-              <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <div className="accordion">
-              What happens if I forget or lose my password?
-            </div>
-            <div className="panel">
-              <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-            </div>
-        </div>
+      )}
+        
     </section>
-  )
-}
+  );
+};
 
 export default DropDownContainer
             
+
+
+
             
           
             
