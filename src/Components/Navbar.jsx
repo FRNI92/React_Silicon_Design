cfe0be7c3b5b5c927cb3f2717621a8742a7c93a9
fredrik <freedrik92@hotmail.com>
2024-11-05 11:14:55 +0100

    gör en push innan jag försöker ändra förstasidan

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,20 @@ import LightLogoType from '../Assets/Bildersilicon/Bildersida1/Desktop-sida1-mob
 import DarkLogoType from '../Assets/Bildersilicon/Bildersida1/Desktop-sida1-mobil/darklogasilicon.svg'
 
 const Navbar = ({ isDarkMode, setIsDarkMode }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
     const handleToggle = () => {
       setIsDarkMode(!isDarkMode);
       // console.log("isDarkMode efter toggling:", !isDarkMode);
     };
 
+    const toggleMenu = () => {
+      console.log("Hamburgermeny klickad");
+      setMenuOpen(!menuOpen);
+    };
+    useEffect(() => {
+      console.log("menuOpen är nu:", menuOpen);
+    }, [menuOpen]);
 
     useEffect(() => {
       if (isDarkMode) {
@@ -25,6 +33,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
               <a href="index.html">
                 <img src={isDarkMode ? DarkLogoType : LightLogoType} alt="Silicon crystal purple gradient with the text Si 14 written on top. Silicon Design" /></a>
                 <a href="#brands" className="feature-text">Features</a>
+                <a href="#brands" className="feature-text">Contact</a>
             </div>
               
             <div className="right-nav">
@@ -37,6 +46,23 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
                 />
                 <span className="slider"></span>
               </label>
+              <div className="menu-container">
+                <div className="hamburger" onClick={toggleMenu}>
+                  ☰ 
+                </div>
+                {menuOpen && (
+                <ul className="menu">
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#features">Features</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                </ul>
+                
+                )}
+              </div>
+              
+
+
+              
               
               <a href="#" className="btn-primary">
                 <i className="fa-solid fa-right-to-bracket"></i>
